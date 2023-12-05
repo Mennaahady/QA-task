a562adc4b43c38f953a709c777edf368dd95b862
Mennaahady <mennaadel62@gmail.com>
2023-12-05 21:14:54 +0200

    Added comments

--- Task.py
+++ Task.py
@@ -1,8 +1,12 @@
+# Import the requests library for making HTTP requests
 import requests
 
+# Define the base URL of the Reqres API
 BASE_URL = "https://reqres.in/api"
-USER_ID = None  # To store the user ID created during the test
+# Global variable to store the user ID created during the test
+USER_ID = None  
 
+# Function to create a user by making a POST request
 def create_user(name, job):
     global USER_ID
     endpoint = f"{BASE_URL}/users"
@@ -12,11 +16,13 @@ def create_user(name, job):
     USER_ID = user_data.get("id")
     return response
 
+# Function to list users by making a GET request
 def list_users():
     endpoint = f"{BASE_URL}/users"
     response = requests.get(endpoint)
     return response
 
+# Function to update a user by making a PUT request
 def update_user(name, job):
     global USER_ID
     if USER_ID is None:
@@ -26,11 +32,12 @@ def update_user(name, job):
     response = requests.put(endpoint, json=data)
     return response
 
+# Function to delete a user by making a DELETE request
 def delete_user():
     global USER_ID
     if USER_ID is None:
         raise ValueError("User ID is not available for deletion. Please create a user first.")
     endpoint = f"{BASE_URL}/users/{USER_ID}"
     response = requests.delete(endpoint)
-    USER_ID = None  # Reset USER_ID after deletion
+    USER_ID = None  
     return response
--- tasktest.py
+++ tasktest.py
@@ -1,26 +1,32 @@
 import pytest
+# Import functions from the Task.py script for testing
 from Task import create_user, list_users, update_user, delete_user
 
+# Define a fixture named user_data to provide test data for user
 @pytest.fixture
 def user_data():
     return {"name": "John Doe", "job": "Engineer"}
 
+# Test function to verify the creation of a user
 def test_create_user(user_data):
     response = create_user(user_data["name"], user_data["job"])
     assert response.status_code == 201
     assert "id" in response.json()
 
+# Test function to verify listing users
 def test_list_users():
     response = list_users()
     assert response.status_code == 200
     assert "data" in response.json()
 
+# Test function to verify updating a user
 def test_update_user(user_data):
     response = update_user(user_data["name"], user_data["job"])
     assert response.status_code == 200
     assert response.json()["name"] == user_data["name"]
     assert response.json()["job"] == user_data["job"]
 
+# Test function to verify deleting a user
 def test_delete_user():
     response = delete_user()
     assert response.status_code == 204
